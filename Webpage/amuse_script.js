6e2b50c49cfa0452d476d21b78bb457f73322fa7
Rattles <Waffles2897@gmail.com>
2026-07-07 23:14:08 -0400

    Added: orderTics() function & hook to order.html <form> onsubmit

---
+++ Webpage/amuse_script.js
@@ -0,0 +1,11 @@
+// Yates | Due July 12, 2026 | Assignment 4
+
+// ===== orderTics() =====
+//Called when the order form is submitted
+// Displays a fixed price alert and prevents form navigation
+
+function orderTics() {
+    alert("Your total price is: $35");
+    return false;
+}
+
