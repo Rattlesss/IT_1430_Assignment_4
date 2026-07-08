aa340b517367c0923efd83e2195fb78fa924b015
Rattles <Waffles2897@gmail.com>
2026-07-07 23:29:30 -0400

    Added: rideInfo() function and onclick handlers to info.html table

--- Webpage/amuse_script.js
+++ Webpage/amuse_script.js
@@ -1,7 +1,9 @@
 // Yates | Due July 12, 2026 | Assignment 4
 
+
+
 // ===== orderTics() =====
-//Called when the order form is submitted
+// Called when the order form is submitted
 // Displays a fixed price alert and prevents form navigation
 
 function orderTics() {
@@ -9,3 +11,10 @@ function orderTics() {
     return false;
 }
 
+// ===== rideInfo() =====
+// Called when a ride name is clicked on info.html
+// Displays the ride's ID and name in an alert
+
+function rideInfo(id, name) {
+    alert("You clicked on the ride with the ID of: " + id + " and name of: " + name)
+}
